5519869c2fa8445bdfd57e75c2b2e3d73fb6bb1e
Josh-Ely <99770155+Josh-Ely@users.noreply.github.com>
2022-04-16 01:43:24 -0400

    Update MainClassForProject.java

--- MainClassForProject.java
+++ MainClassForProject.java
@@ -8,16 +8,17 @@ public class MainClassForProject {
 
         boolean exit = false;
         do {
-            System.out.println("1: Search for a book");
+            System.out.println("1: Search for a Book");
             System.out.println("2: Show Current Book Inventory");
             System.out.println("3: Add a Book");
             System.out.println("4: Issue a Book");
-            System.out.println("5: Pay Fees");
-            System.out.println("6: Exit");
+            System.out.println("5: Edit or Delete Book");
+            System.out.println("6: Pay Fees");
+            System.out.println("7: Exit");
             System.out.print("Enter a Menu Option: ");
             Scanner in = new Scanner(System.in);
             int reponse = in.nextInt();
-            if (reponse < 1 || reponse > 6) {
+            if (reponse < 1 || reponse > 7) {
                 System.out.println("Enter the correct menu option below");
             }
             switch (reponse) {
@@ -34,9 +35,12 @@ public class MainClassForProject {
                     System.out.println("user can issue a book here");
                     break;
                 case 5:
-                    System.out.println("fees can be displayed and invoices can be printed here");
+                    System.out.println("user can edit or delete books here");
                     break;
                 case 6:
+                    System.out.println("fees or invoices can be displayed here");
+                    break;
+                case 7:
                     exit = true;
                     System.out.println("Have a nice day :)");
                     break;
@@ -46,3 +50,4 @@ public class MainClassForProject {
 
     }
 }
+
